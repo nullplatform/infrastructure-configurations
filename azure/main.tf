888660636c93d9425ae9042a83946715d04d6303
David Cohan <cohandv@gmail.com>
2024-11-05 16:32:22 -0300

    added creation of acr

--- azure/main.tf
+++ azure/main.tf
@@ -80,3 +80,13 @@ module "nullplatform" {
     module.aks
   ]
 }
+
+module "my_apps" {
+  for_each = var.application_names
+
+  source = "./modules/azure/acr"
+
+  application_name    = each.key
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+}
