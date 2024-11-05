4ec69804d204be42a0f65ab35e293278c1f8f70f
David Cohan <cohandv@gmail.com>
2024-11-05 10:59:18 -0300

    remove hardcoded values

--- azure/modules/nullplatform-azure/main.tf
+++ azure/modules/nullplatform-azure/main.tf
@@ -1,44 +1,44 @@
 resource "nullplatform_provider_config" "azure" {
-  provider   = nullplatform
-  account    = "ey-poc-main"
-  type       = "azure-configuration"
-  dimensions = {}
-  attributes = jsonencode({
-    "authentication" : {
-      "client_id" : "6307dea1-d717-4ffc-9aa7-ce2f665d34f6",
-      "tenant_id" : "ebf65bf9-2e0c-49f0-88a9-8cb51e713016",
-      "client_secret" : "s2o8Q~_jW8rUSTdwtuA3khep-y-v_Cs20dtoudys",
-      "subscription_id" : "4f01781f-c243-43cd-bf9a-5532f6379f9d"
+  provider      = nullplatform
+  account       = var.account
+  type          = "azure-configuration"
+  dimensions    = {}
+  attributes    = jsonencode({
+    "authentication": {
+      "client_id": "6307dea1-d717-4ffc-9aa7-ce2f665d34f6",
+      "tenant_id": "ebf65bf9-2e0c-49f0-88a9-8cb51e713016",
+      "client_secret": "s2o8Q~_jW8rUSTdwtuA3khep-y-v_Cs20dtoudys",
+      "subscription_id": "4f01781f-c243-43cd-bf9a-5532f6379f9d"
     },
-    "networking" : {
-      "domain_name" : "ey-poc.nullapps.io",
-      "public_dns_zone_name" : "ey-poc.nullapps.io",
-      "public_dns_zone_resource_group_name" : "rg-ey-demo-main",
+    "networking": {
+      "domain_name": "${var.account}.nullapps.io",
+      "public_dns_zone_name": "${var.account}.nullapps.io",
+      "public_dns_zone_resource_group_name": "rg-${var.account}-main",
     },
-    }
+  }
   )
 }
 
 resource "nullplatform_provider_config" "aks" {
-  provider   = nullplatform
-  account    = "ey-poc-main"
-  type       = "aks-configuration"
-  dimensions = {}
+  provider     = nullplatform
+  account      = var.account
+  type         = "aks-configuration"
+  dimensions   = {}
   attributes = jsonencode({
-    "cluster" : {
-      "id" : "ey-demo-main-cluster",
-      "namespace" : "nullplatform",
-      "resource_group" : "rg-ey-demo-main",
+    "cluster": {
+      "id": "${var.account}-main-cluster",
+      "namespace": "nullplatform",
+      "resource_group": "rg-${var.account}-main",
     },
-    "gateway" : {
-      "namespace" : "gateways",
-      "public_name" : "gateway-public",
+    "gateway": {
+      "namespace": "gateways",
+      "public_name": "gateway-public",
     },
-    "networking" : {
-      "public_balancer_ip" : "172.179.53.237",
-      "balancer_ip" : "10.0.1.6",
+    "networking": {
+      "public_balancer_ip": "172.179.53.237",
+      "balancer_ip": "10.0.1.6",
     },
-    "web_pool_provider" : "AZURE:WEB_POOL:AKS"
+    "web_pool_provider": "AZURE:WEB_POOL:AKS"
   })
   depends_on = [
     nullplatform_provider_config.azure
