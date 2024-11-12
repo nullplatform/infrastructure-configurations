93a2bc0a76aa0577dd44ff541e2ce1a559432465
David Cohan <cohandv@gmail.com>
2024-11-12 11:39:50 -0300

    dont use account subdomain

--- public/azure/modules/nullplatform/providers/config/main.tf
+++ public/azure/modules/nullplatform/providers/config/main.tf
@@ -13,6 +13,7 @@ resource "nullplatform_provider_config" "azure" {
       "domain_name" : "${var.account}.nullapps.io",
       "public_dns_zone_name" : var.domain_name,
       "public_dns_zone_resource_group_name" : var.azure_resource_group_id,
+      "application_domain": false
     },
     }
   )
