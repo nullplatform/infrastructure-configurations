8ef2a1c36579cd8b31b48bfb867d11c88b7413bf
Luis Brito <luis.brito@digitalfemsa.com>
2024-12-05 17:01:19 -0600

    Add back genesys_admin role

--- aws/modules/eks-cluster/main.tf
+++ aws/modules/eks-cluster/main.tf
@@ -82,6 +82,11 @@ module "eks" {
       username = "AWSReservedSSO_Owner_Access_SSO_ed1fea9453f242c7"
       groups   = ["system:masters"]
     },
+    {
+      rolearn  = "arn:aws:iam::481665089566:role/genesys_admin"
+      username = "481665089566"
+      groups   = ["system:masters"]
+    },
   ]
 
   tags = {
